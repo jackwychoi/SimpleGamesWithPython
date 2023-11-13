01943572cacfc54cccc89bff37b85df28e93a837
WONYOUNG CHOI <83526329+jackwychoi@users.noreply.github.com>
2023-11-13 20:49:31 +0900

    commit on 2023-11-13

---
+++ practice_date_simple.py
@@ -0,0 +1,8 @@
+import datetime
+
+today = datetime.datetime.now()
+
+print('Today is', today)
+print('Year of today is', today.year)
+print('Month of today is', today.month)
+print('Day of today is', today.day)
