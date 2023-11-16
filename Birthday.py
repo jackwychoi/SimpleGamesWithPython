bfb834d7e902d1824dcde1bcb6b7ed4b1016d990
WONYOUNG CHOI <83526329+jackwychoi@users.noreply.github.com>
2023-11-16 21:32:58 +0900

    commit on 2023-11-16

---
+++ Birthday.py
@@ -0,0 +1,27 @@
+import datetime
+
+year = input('What year were you born?')
+year = int(year)
+
+month = input('What month were you born?')
+month = int(month)
+
+day = input('What day were you born?')
+day = int(day)
+
+bday = datetime.datetime(year, month, day)
+
+if bday.weekday() == 6:
+    print('You were born on Sunday!')
+elif bday.weekday() == 0:
+    print('You were born on Monday!')
+elif bday.weekday() == 1:
+    print('You were born on Tuesday!')
+elif bday.weekday() == 2:
+    print('You were born on Wednesday!')
+elif bday.weekday() == 3:
+    print('You were born on Thursday!')
+elif bday.weekday() == 4:
+    print('You were born on Friday!')
+elif bday.weekday() == 5:
+    print('You were born on Saturday!')
