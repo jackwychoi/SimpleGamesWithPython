bcbd1f90b5ab637584b84d80b3b101e13c0d31f5
WONYOUNG CHOI <83526329+jackwychoi@users.noreply.github.com>
2023-11-22 00:07:27 +0900

    commit on 2023-11-22

---
+++ practice_loop02.py
@@ -0,0 +1,13 @@
+userInput = input('Type anything first / Type "STOP" to stop')\
+    .upper().strip()
+
+while userInput != "STOP":
+    userInput = input('Type anything first / Type "STOP" to stop')\
+        .upper().strip()
+    
+# Simplified Ver.
+userInput = ''
+
+while userInput != "STOP":
+    userInput = input('Type anything first / Type "STOP" to stop')\
+        .upper().strip()
